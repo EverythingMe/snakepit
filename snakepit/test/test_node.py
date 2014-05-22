098038b1e2f0625201717a489c7090ffd8470263
Roey Berman <roey@everything.me>
2014-05-22 13:59:40 +0300

    implement mocks for tests

--- snakepit/test/test_node.py
+++ snakepit/test/test_node.py
@@ -4,9 +4,8 @@ from ..node import Node
 from ..pool import Pool
 from ..registry import StaticRegistry
 
+from collections import defaultdict
 from unittest import TestCase
-
-__author__ = 'bergundy'
 
 
 class NetworklessNode(Node):
@@ -15,15 +14,23 @@ class NetworklessNode(Node):
 
 
 class LocalPool(Pool):
+    def __init__(self):
+        self._peers = defaultdict(dict)
+
     def call(self, peer, callName, *args, **kwargs):
-        pass
+        getattr(self._peers[peer], callName)(*args, **kwargs)
 
 
 class NodeTestCase(TestCase):
     def setUp(self):
-        self.node = NetworklessNode('local', )
+        self.endpoint = 'local'
+        self.registry = StaticRegistry('Test')
+        self.pool = LocalPool()
+        self.handler = dict()
+        self.node = NetworklessNode(self.endpoint, self.registry, self.pool, self.handler)
 
     def testDoLocalNode(self):
         pass
+
 
 
